a9e91e63ed35b190294d10244d6046940cdf7e6d
Gabriel Rodrigues <gabrielmendesrodriguest@hotmail.com>
2025-03-27 12:43:41 -0300

    created Create/Edit page

--- ExpenseTracker/ExpenseTracker/Controllers/HomeController.cs
+++ ExpenseTracker/ExpenseTracker/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace ExpenseTracker.Controllers
             return View();
         }
 
+        public IActionResult CreateEdit()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
